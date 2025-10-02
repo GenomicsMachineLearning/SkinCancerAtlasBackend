5f1a24656ec27c34d7741d2c9c2ef71c3a395525
Andrew Newman <andrewfnewman@gmail.com>
2025-10-02 17:34:50 +1000

    Revert.

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,18 +3,3 @@ from app.main import app
 
 # Create the Mangum handler
 handler = Mangum(app, lifespan="off")
-
-def lambda_handler(event, context):
-    # Call the Mangum handler
-    response = handler(event, context)
-
-    # Ensure headers dict exists
-    if 'headers' not in response:
-        response['headers'] = {}
-
-    # Force add CORS headers to every response
-    response['headers']['access-control-allow-origin'] = '*'
-    response['headers']['access-control-allow-methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
-    response['headers']['access-control-allow-headers'] = '*'
-
-    return response
